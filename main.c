0f839a57690fed9debff6866b48555b822341b1a
Darkmagicion <93094590+Darkmagicionx@users.noreply.github.com>
2021-11-08 14:54:49 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define BUFFER_SIZE (2)
+int main()
+{
+	// r - Opens to read
+	FILE* fp = fopen("file.txt", "r");
+	
+	if(fp == NULL)
+	{
+		printf("Error opening the file\n");
+		return 1;
+	}
+	
+	
+	
+	char buffer[BUFFER_SIZE];
+	int reads = 0;
+	do
+	{
+		if(fgets(buffer, BUFFER_SIZE, fp) == NULL)
+			break;
+			
+		reads++;
+		
+		printf("%s", buffer);
+		
+	}while(1);
+		
+	printf("reads: %d | EOF: %d\n", reads, EOF);
+	fclose(fp);
+	return 0;
+}
